68dc70ea140acb552890248830802fdd91aa9045
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-11-23 08:42:45 +0100

    Add files via upload

---
+++ GeometrijskoTijelo_nasljedivanje/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrijskoTijelo_nasljedivanje
+{
+    class GeometrijskoTijelo
+    {
+        double volumet, oplosje;
+        public static int brojacTijela = 0;
+    }
+    class Kugla : GeometrijskoTijelo
+    {
+        static Kugla()
+        {
+            brojacTijela += 1;
+        }
+    }
+    class Kvadar : GeometrijskoTijelo
+    {
+        static Kvadar()
+        {
+            brojacTijela+=1;
+        }
+    }
+       class Kocka: Kvadar
+       {
+            static Kocka()
+            {
+                brojacTijela+=1;
+            }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Kvadar y = new Kvadar();
+            Kocka z = new Kocka();
+
+
+            Console.WriteLine("Definirana broji tijela je: " + GeometrijskoTijelo.brojacTijela);
+            Console.ReadKey();
+        }
+    }
+}
